75306f1f341ea6006f80db320f6f658140ef0f90
ImDamianDev <imdamian.dev@gmail.com>
2023-02-15 23:04:49 -0300

    style footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 //import styles
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands} from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 function Footer() {
   return (
@@ -7,8 +9,8 @@ function Footer() {
       <div className="container">
         <footer className="py-3">
           <ul className="nav justify-content-center pb-3 mb-3">
-            <li className="nav-item"><a href="https://github.com/ImDamianDev/" className="nav-link"><i className="bi bi-github"></i></a></li>
-            <li className="nav-item"><a href="https://github.com/ImDamianDev/" className="nav-link">LinkedIn</a></li>
+            <li className="nav-item"><a href="https://github.com/ImDamianDev/" className="nav-link"><FontAwesomeIcon icon={brands('github')} size="2x"/></a></li>
+            <li className="nav-item"><a href="https://www.linkedin.com/in/imdamian-dev/" className="nav-link"><FontAwesomeIcon icon={brands('linkedin')} size="2x"/></a></li>
           </ul>
           <p className="text-center">Con cariño desde Chile</p>
         </footer>
