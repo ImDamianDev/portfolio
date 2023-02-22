4903e3bb4746d0e2209dfc41f17b810e12df3ae9
ImDamianDev <imdamian.dev@gmail.com>
2023-02-22 13:11:35 -0300

    separate proyect card component

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,42 +1,32 @@
 //import styles
 import './Works.css';
+import WorksCard from './WorksCard'
+
+const proyects = [
+  {
+    nombre_proyecto: "react-poke-app",
+    repo_url: "https://github.com/ImDamianDev/react-poke-app",
+    web_url: "https://imdamiandev-react-poke-app.netlify.app/"
+  },
+  {
+    nombre_proyecto: "react-calculator",
+    repo_url: "https://github.com/ImDamianDev/react-calculator",
+    web_url: "https://imdamiandev-react-calculator.netlify.app/"
+  }
+
+]
+
 
 function Works() {
 
   return (
-    <div id="works_section" className="container-fluid px-4 py-5">
-      <h2 className="title-section pb-2">Works</h2>
+    <div id="works_section" className="container-fluid px-4 py-4">
+      <h2 className="section-title pb-2 mb-4">Works</h2>
 
-      <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-
-        <div id="react-poke-app" className="card h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
-          <div className="content">
-            <h2 className="title">React-Poke-App</h2>
-            <a href="https://github.com/ImDamianDev/react-poke-app"
-            type="button" className="btn">
-              View Repo
-            </a>
-            <a href="https://imdamiandev-react-poke-app.netlify.app/"
-            type="button" className="btn">
-              View Website
-            </a>
-          </div>
-        </div>
-
-        <div id="react-calculator" className="card h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
-          <div className="content">
-            <h2 className="title">React-Calculator</h2>
-            <a href="https://github.com/ImDamianDev/react-calculator"
-            type="button" className="btn">
-              View Repo
-            </a>
-            <a href="https://imdamiandev-react-calculator.netlify.app/"
-            type="button" className="btn">
-              View Website
-            </a>
-          </div>
-        </div>
-
+      <div className="row">
+        {proyects.map( (proyect) =>
+        <WorksCard data={proyect} />
+        )}
       </div>
 
     </div>
---
+++ src/components/WorksCard.js
@@ -0,0 +1,26 @@
+//import styles
+import './WorksCard.css';
+
+function WorksCard(props) {
+
+  return (
+    <div className="col-12 col-md-6 col-lg-4 mb-4 mb-md-0">
+      <div id={props.data.nombre_proyecto} className="card-proyect">
+        <h2 className="proyect-title">{props.data.nombre_proyecto}</h2>
+        <div className="proyect-links">
+          <a href={props.data.repo_url}
+            type="button" className="btn">
+            Repo
+          </a>
+          <a href={props.data.web_url}
+            type="button" className="btn">
+            Website
+          </a>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default WorksCard;
