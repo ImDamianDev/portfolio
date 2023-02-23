93559492227593bcd2d265c1609a7df07bb79faa
imdamiandev <imdamian.dev@gmail.com>
2023-02-23 01:25:27 -0300

    add chatbot proyect

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,8 @@ function Footer() {
             <li className="nav-item"><a href="https://www.linkedin.com/in/imdamian-dev/" className="nav-link"><FontAwesomeIcon icon={brands('linkedin')} size="2x"/></a></li>
             <li className="nav-item"><a href="mailto:imdamian.dev@gmail.com" className="nav-link"><FontAwesomeIcon icon={solid('envelope')} size="2x"/></a></li>
           </ul>
-          <p className="text-center">Con cariño desde Chile</p>
+          <p className="text-center">Con &#x2665; desde Chile.</p>
+          <p className="text-center">&#169; 2023 - Todos los derechos reservados.</p>
         </footer>
       </div>
     </div>
--- src/components/Works.js
+++ src/components/Works.js
@@ -12,6 +12,11 @@ const proyects = [
     nombre_proyecto: "react-calculator",
     repo_url: "https://github.com/ImDamianDev/react-calculator",
     web_url: "https://imdamiandev-react-calculator.netlify.app/"
+  },
+  {
+    nombre_proyecto: "react-chatbot-app",
+    repo_url: "https://github.com/ImDamianDev/react-chatbot-app.git",
+    web_url: "https://imdamiandev-react-chatbot-app.netlify.app/"
   }
 
 ]
