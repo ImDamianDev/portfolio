8c43e2d65fcad26b2465e756685316efe368083a
imdamiandev <imdamian.dev@gmail.com>
2023-02-28 22:27:55 -0300

    separate proyect list from data

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,26 +1,7 @@
 //import styles
 import './Works.css';
 import WorksCard from './WorksCard'
-
-const proyects = [
-  {
-    nombre_proyecto: "react-poke-app",
-    repo_url: "https://github.com/ImDamianDev/react-poke-app",
-    web_url: "https://imdamiandev-react-poke-app.netlify.app/"
-  },
-  {
-    nombre_proyecto: "react-calculator",
-    repo_url: "https://github.com/ImDamianDev/react-calculator",
-    web_url: "https://imdamiandev-react-calculator.netlify.app/"
-  },
-  {
-    nombre_proyecto: "react-chatbot-app",
-    repo_url: "https://github.com/ImDamianDev/react-chatbot-app.git",
-    web_url: "https://imdamiandev-react-chatbot-app.netlify.app/"
-  }
-
-]
-
+import proyects from '../data/proyects'
 
 function Works() {
 
---
+++ src/data/proyects.js
@@ -0,0 +1,20 @@
+const proyects = [
+  {
+    nombre_proyecto: "react-poke-app",
+    repo_url: "https://github.com/ImDamianDev/react-poke-app",
+    web_url: "https://imdamiandev-react-poke-app.netlify.app/"
+  },
+  {
+    nombre_proyecto: "react-calculator",
+    repo_url: "https://github.com/ImDamianDev/react-calculator",
+    web_url: "https://imdamiandev-react-calculator.netlify.app/"
+  },
+  {
+    nombre_proyecto: "react-chatbot-app",
+    repo_url: "https://github.com/ImDamianDev/react-chatbot-app.git",
+    web_url: "https://imdamiandev-react-chatbot-app.netlify.app/"
+  }
+
+]
+
+export default proyects;
