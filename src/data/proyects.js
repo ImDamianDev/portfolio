c85d574180218fce99cf52aad14175d3591a5534
ImDamianDev <imdamian.dev@gmail.com>
2023-09-04 14:17:23 -0300

    update proyect-list

--- src/data/proyects.js
+++ src/data/proyects.js
@@ -15,11 +15,6 @@ const proyects = [
     web_url: "https://imdamiandev-react-chatbot-app.netlify.app/"
   },
   {
-    nombre_proyecto: "re-cursos-dev",
-    repo_url: "https://github.com/ImDamianDev/re-cursos-dev.git",
-    web_url: "https://re-cursos-dev.vercel.app/"
-  },
-  {
     nombre_proyecto: "next-ecommmerce",
     repo_url: "https://github.com/ImDamianDev/next_ecommerce.git",
     web_url: "https://next-ecommerce-ruddy-one.vercel.app/"
